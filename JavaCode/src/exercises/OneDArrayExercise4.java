b5edfbea947924ba7112fb6832fd2a736f9fe005
Min-Thant794 <minthanttun@kaminarimanfukyuus-MacBook-Pro.local>
2026-03-17 16:01:03 +0800

    updated

--- JavaCode/src/exercises/OneDArrayExercise4.java
+++ JavaCode/src/exercises/OneDArrayExercise4.java
@@ -5,8 +5,35 @@ import static exercises.OneDArrayExercise2.setData;
 public class OneDArrayExercise4 {
     public static void main(String[] args)
     {
-        int size = getSize();
-        int[] arr = new int[size];
-        setData(arr);
+        int size1 = getSize();
+        int[] arr1 = new int[size1];
+        setData(arr1);
+
+        int size2 = getSize();
+        int[] arr2 = new int[size2];
+        setData(arr2);
+
+        boolean isIdentical = isIdentical(arr1, arr2);
+
+        if (isIdentical) {
+            System.out.println("This 2 arrays are identical");
+        } else {
+            System.out.println("This 2 arrays are not identical");
+        }
+    }
+
+    public static boolean isIdentical(int[] a1, int[] a2)
+    {
+        if (a1.length != a2.length) {
+            return false;
+        }
+
+        for (int i = 0; i < a1.length; i++) {
+            if(a1[i] != a2[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
